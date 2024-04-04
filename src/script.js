52b8a7d3d44f45a4801138ca50a3b014a4a7f494
ZaneEason <zseason@iastate.edu>
2024-04-04 16:48:04 -0500

    Got most of the first 2 views done, still need input validation

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, {useState, useEffect} from "react";
 import {Products} from "./Products";
+import {useForm} from "react-hook-form";
 
 //VIEW PRODUCTS AND SEARCH HERE
 const Script = () => {
@@ -9,6 +10,8 @@ const Script = () => {
     const [query, setQuery] = useState('');
     const [cart, setCart] = useState([]);
     const [cartTotal, setCartTotal] = useState(0);
+    const [viewer,setViewer] = useState(0);
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
     useEffect(() => {
         total();
@@ -37,6 +40,13 @@ const Script = () => {
         }
     }
 
+    const cartItems = cart.map((el) => (
+        <div key={el.id}>
+                {el.title}{" = "}
+                ${el.price}
+        </div>
+    ));
+
     const total = () => {
         let totalVal = 0;
         for (let i = 0; i < cart.length; i++){
@@ -55,8 +65,151 @@ const Script = () => {
         setProductsCategory(results);
     }
 
+    const checkout = () => {
+        setViewer(1);
+    }
+
+    const order = () => {
+        setViewer(2);
+    }
+
+    const confirm = () => {
+        setViewer(0);
+        setCart([]);
+        setCartTotal(0);
+    }
+
+    const return_to_products = () => {
+        setViewer(0);
+    }
+
+    const return_to_cart = () => {
+        setViewer(1);
+    }
+
+    if (viewer == 1){
+        return (
+            <div className="flex fixed flex-row">
+                {console.log("Step 2 : Return App :",Products.length,ProductsCategory.length)}
+                <div className="h-screen  bg-slate-800 p-3 xl:basis-1/5" style={{ minWidth: '65%'}}>
+                    <div className="px-6 py-4">
+                    <h1 className="text-3xl mb-2 font-bold text-white"> COM S 319 Assignment 2 </h1>
+                    <p className="text-gray-700 text-white">
+                        by - <b style={{ color: 'orange' }}>Zane Eason, Benjamin Diaz</b>
+                    </p>
+            
+                    <span class ="text-gray-700 text-white">Cart total:   </span>
+                    <span class ="text-gray-700 text-white">${cartTotal}</span>
+
+                    
+        <div>
+            <form onSubmit={handleSubmit(order)} className="container mt-5">
+                <div className="form-group">
+                    <input {...register("fullName", { required: true })} placeholder="Full Name" className="form-control"/>
+                    {errors.fullName && <p className="text-danger">Full Name is required.</p>}
+                </div>
+
+                <div className="form-group">
+                    <input {...register("email", { required: true, pattern: /^\S+@\S+$/i })} placeholder="Email" className="form-control"/>
+                    {errors.email && <p className="text-danger">Email is required.</p>}
+                </div>
+
+                <div className="form-group">
+                    <input {...register("creditCard", { required: true })} placeholder="Credit Card" className="form-control"/>
+                    {errors.creditCard && <p className="text-danger">Credit Card is required.</p>}
+                </div>
+
+                <div className="form-group">
+                    <input {...register("address", { required: true })} placeholder="Address" className="form-control"/>
+                    {errors.address && <p className="text-danger">Address is required.</p>}
+                </div>
+
+                <div className="form-group">
+                    <input {...register("address2")} placeholder="Address 2" className="form-control"/>
+                </div>
+
+                <div className="form-group">
+                    <input {...register("city", { required: true })} placeholder="City" className="form-control"/>
+                    {errors.city && <p className="text-danger">City is required.</p>}
+                </div>
+
+                <div className="form-group">
+                    <input {...register("state", { required: true })} placeholder="State" className="form-control"/>
+                    {errors.state && <p className="text-danger">State is required.</p>}
+                </div>
+
+                <div className="form-group">
+                    <input {...register("zip", { required: true })} minlength="5" maxlength="5" placeholder="Zip" className="form-control"/>
+                    {errors.zip && <p className="text-danger">Zip is required.</p>}
+                </div>
+                    <button type="button" className = "btn btn-secondary" variant="light" onClick={() => return_to_products()}> Return</button>
+
+                <button type="submit" className="btn btn-primary">Order</button>
+            </form>
+                    </div>
+
+                    </div>
+                </div>
+                <div className="ml-5  p-10 xl:basis-4/5">
+            <div className='category-section fixed'>
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Cart ({cart.length})</h2>
+                <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-10" style={{ maxHeight: '550px', overflowY: 'scroll' }}>
+                    {cart.map((product, index) => (
+                        <div key={index} className="group relative shadow-lg" >
+                            <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
+                                <img
+                                    alt="Product Image"
+                                    src={product.image}
+                                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                                />
+                            </div>
+                            <div className="flex justify-between p-3">
+                                <div>
+                                    <h3 className="text-sm text-gray-700">
+                                        <a href={product.href}>
+                                            <span style={{ fontSize: '16px', fontWeight: '600' }}>{product.title}</span>
+                                        </a>
+                                    </h3>
+                                    <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate}</p>
+                                    <p className="text-sm font-medium text-green-600">${product.price}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+            </div>
+            );
+    }
+    else if (viewer == 2){
+        return (
+            <div className="flex fixed flex-row">
+                {console.log("Step 2 : Return App :",Products.length,ProductsCategory.length)}
+                <div className="h-screen  bg-slate-800 p-3 xl:basis-1/5" style={{ minWidth: '65%' }}>
+                    <div className="px-6 py-4">
+                    <h1 className="text-3xl mb-2 font-bold text-white"> COM S 319 Assignment 2 </h1>
+                    <p className="text-gray-700 text-white">
+                        by - <b style={{ color: 'orange' }}>Zane Eason, Benjamin Diaz</b>
+                    </p>
+                    <span class ="text-gray-700 text-white">Please confirm your order</span> <br></br> <br></br>
+
+                    <button type="button" className = "btn btn-secondary" variant="light" onClick={() => return_to_cart()}> Return</button>
+                    <button type="button" className = "btn btn-primary" variant="light" onClick={() => confirm()}> Confirm</button>
+                    
+                    </div>
+                </div>
+                <div className="ml-5  p-10 xl:basis-4/5">
+
+                {/*YOUR CODE GOES HERE FOR PAYMENT/CONFIRMATION PAGE*/}
+            
+                </div>
+            </div>
+        );
+    }
+    else if (viewer == 0){
     return (
-        <div className="flex fixed flex-row">
+    <div className="flex fixed flex-row">
 	    {console.log("Step 2 : Return App :",Products.length,ProductsCategory.length)}
         <div className="h-screen  bg-slate-800 p-3 xl:basis-1/5" style={{ minWidth: '65%' }}>
             <div className="px-6 py-4">
@@ -70,16 +223,15 @@ const Script = () => {
                 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
                 dark:focus:ring-blue-500 dark:focus:border-blue-500" type="search" value={query} onChange={handleChange} />
             </div>
-            
-            <span class ="text-gray-700 text-white">Order total:   </span>
-            <span class ="text-gray-700 text-white">${cartTotal}</span>
+
+            <button type="button" className = "btn btn-primary" variant="light" onClick={() => checkout()}> Checkout </button>
             </div>
         </div>
         <div className="ml-5  p-10 xl:basis-4/5">
             {console.log("Before render :",Products.length,ProductsCategory.length)}
             <div className='category-section fixed'>
                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Products ({ProductsCategory.length})</h2>
-                <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-5 xl:gap-x-10" style={{ maxHeight: '800px', overflowY: 'scroll' }}>
+                <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-10" style={{ maxHeight: '550px', overflowY: 'scroll' }}>
                     {ProductsCategory.map((product, index) => (
                         <div key={index} className="group relative shadow-lg" >
                             <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
@@ -93,17 +245,15 @@ const Script = () => {
                                 <div>
                                     <h3 className="text-sm text-gray-700">
                                         <a href={product.href}>
-                                            <span aria-hidden="true" className="absolute inset-0" />
                                             <span style={{ fontSize: '16px', fontWeight: '600' }}>{product.title}</span>
                                         </a>
-                                        <p>Tag - {product.category}</p>
                                     </h3>
                                     <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate}</p>
-                                    <button type="button" variant="light" onClick={() => removeFromCart(product)} > - </button>{" "}
-                                    <button type="button" variant="light" onClick={() => addToCart(product)}> + </button>
-                                    ${product.price} <span class="close">&#10005;</span>{howManyofThis(product.id)}
+                                    <p className="text-sm font-medium text-green-600">${product.price}</p>
+                                    <button type="button" className="btn btn-secondary" variant="light" onClick={() => removeFromCart(product)} > - </button>{" "}
+                                    <button type="button" className="btn btn-secondary" variant="light" onClick={() => addToCart(product)}> + </button>
+                                    ${product.price} <span class="close">&#10005; </span>{howManyofThis(product.id)}
                                 </div>
-                                <p className="text-sm font-medium text-green-600">${product.price}</p>
                             </div>
                         </div>
                     ))}
@@ -112,6 +262,7 @@ const Script = () => {
         </div>
     </div>
     );
+    }
 }
 
 export default Script;
