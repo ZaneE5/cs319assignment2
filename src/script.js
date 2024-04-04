73c2721cb35d9c37c112cc21cf71445fe3cfa600
ZaneEason <zseason@iastate.edu>
2024-04-04 13:05:18 -0500

    Initial commit with some base code from in-class assignments

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 import './App.css';
-import logo from './logo.png';
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {Products} from "./Products";
 import {Categories} from "./Categories";
 
@@ -31,6 +30,8 @@ const render_products = (ProductsCategory) => {
                 <p>Tag - {product.category}</p>
               </h3>
               <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate}</p>
+
+              
             </div>
             <p className="text-sm font-medium text-green-600">${product.price}</p>
           </div>
@@ -40,7 +41,11 @@ const render_products = (ProductsCategory) => {
   </div>
 }; // end render_products
 
-const App = () => {
+
+
+
+//VIEW PRODUCTS AND SEARCH HERE
+const Script = () => {
   console.log("Step 1 : load Products in a useState.");
   const [ProductsCategory, setProductsCategory] = useState(Products);
   const [query, setQuery] = useState('');
@@ -68,7 +73,6 @@ const handleChange = (e) => {
     <div className="flex fixed flex-row">
 	{console.log("Step 2 : Return App :",Products.length,ProductsCategory.length)}
       <div className="h-screen  bg-slate-800 p-3 xl:basis-1/5" style={{ minWidth: '65%' }}>
-        <img className="w-full" src={logo} alt="Sunset in the mountains" />
         <div className="px-6 py-4">
           <h1 className="text-3xl mb-2 font-bold text-white"> Product Catalog App </h1>
           <p className="text-gray-700 text-white">
@@ -97,4 +101,108 @@ dark:focus:ring-blue-500 dark:focus:border-blue-500" type="search" value={query}
 }
 
 
-export default App;
+//CART FUNCTIONALITY HERE
+const Shop = () => {
+    const [cart, setCart] = useState([]);
+    const [cartTotal, setCartTotal] = useState(0);
+
+    useEffect(() => {
+        total();
+    }, [cart]);
+
+    function howManyofThis(id) {
+        let hmot = cart.filter((cartItem) => cartItem.id === id);
+        return hmot.length;
+    }
+
+    const listItems = Products.map((el) => (
+        // PRODUCT
+        <div class="row border-top border-bottom" key={el.id}>
+        <div class="row main align-items-center">
+        <div class="col-2">
+        <img class="img-fluid" src={el.image} />
+        </div>
+        <div class="col">
+        <div class="row text-muted">{el.title}</div>
+        <div class="row">{el.category}</div>
+        </div>
+        <div class="col">
+        <button type="button" variant="light" onClick={() => removeFromCart(el)} > - </button>{" "}
+        <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
+        </div>
+        <div class="col">
+        ${el.price} <span class="close">&#10005;</span>{howManyofThis(el.id)}
+        </div>
+        </div>
+        </div>
+        ));
+
+    const addToCart = (el) => {
+        setCart([...cart, el]);
+    }
+
+    const removeFromCart = (el) => {
+        let itemFound = false;
+        const updatedCart = cart.filter((cartItem) => {
+            if (cartItem.id === el.id && !itemFound) {
+                itemFound = true;
+                return false;
+            }
+            return true;
+        });
+        if (itemFound) {
+            setCart(updatedCart);
+        }
+    }
+
+    const cartItems = cart.map((el) => (
+        <div key={el.id}>
+            <img class="img-fluid" src={el.image} width={150} />
+                {el.title}
+                ${el.price}
+        </div>
+    ));
+
+    const total = () => {
+        let totalVal = 0;
+        for (let i = 0; i < cart.length; i++){
+            totalVal += cart[i].price;
+        }
+        setCartTotal(totalVal);
+    };
+
+    return (
+        <div>
+        STORE SE/ComS319
+        <div class="card">
+        <div class="row">
+        {/* HERE, IT IS THE SHOPING CART */}
+        <div class="col-md-8 cart">
+        <div class="title">
+        <div class="row">
+        <div class="col">
+        <h4>
+        <b>319 Shopping Cart</b>
+        </h4>
+        </div>
+        <div class="col align-self-center text-right text-muted">
+        Products selected {cart.length}
+        </div>
+        </div>
+        </div>
+        <div>{listItems}</div>
+        </div>
+        <div class ="float-end">
+        <p class ="mb-0 me-5 d-flex align-items-center">
+        <span class ="small text-muted me-2">Order total:</span>
+        <span class ="lead fw-normal">${cartTotal}</span>
+        </p>
+        </div>
+        </div>
+        </div>
+        </div>
+        );
+};
+
+//export default Script;
+export default Shop;
